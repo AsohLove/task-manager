e2c8428027ad76a27268ffa46c58f7523bd71199
Love Asoh <loveasoh@gmail.com>
2026-03-17 14:28:56 +0100

    feat: modify task design page

--- src/features/tasks/routes/tasks-page.tsx
+++ src/features/tasks/routes/tasks-page.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import TaskList from "../components/TaskList";
 
-
 type Task = {
   id: number;
   title: string;
-  status: "Completed" | "In Progress" | "Pending";
+  status: "Completed" | "In Progress" | "Pending" | "Overdue";
 };
 
 const dummyTasks: Task[] = [
@@ -14,41 +13,73 @@ const dummyTasks: Task[] = [
   { id: 3, title: "Apply GSOC", status: "Pending" },
   { id: 4, title: "Learn React", status: "In Progress" },
   { id: 5, title: "Cook Yams", status: "Pending" },
+  { id: 6, title: "Bash Projects", status: "Overdue" },
 ];
 
 export default function TasksPage() {
   const [filter, setFilter] = useState("All");
+  const [tasks, setTasks] = useState<Task[]>(dummyTasks);
+  const [view, setView] = useState<"list" | "card">("list");
 
   const filteredTasks =
-    filter === "All"
-      ? dummyTasks
-      : dummyTasks.filter((task) => task.status === filter);
+    filter === "All" ? tasks : tasks.filter((task) => task.status === filter);
+
+  const deleteTasks = (id: number) => {
+    setTasks((prevTasks: Task[]) => prevTasks.filter((task) => task.id !== id));
+  };
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Tasks</h1>
-
-    
-      <div className="flex gap-2 mb-6">
-        {["All", "Completed", "In Progress", "Pending"].map((status) => (
-          <button
-            key={status}
-            type="button"
-            aria-pressed={filter === status}
-            onClick={() => setFilter(status)}
-            className={`px-4 py-1 rounded-full font-medium border cursor-pointer ${
-              filter === status
-                ? "bg-blue-500 text-white border-blue-500"
-                : "bg-white text-gray-700 border-gray-300"
-            }`}
-          >
-            {status}
-          </button>
-        ))}
+    <div className="p-6 rounded bg-gray-100 h-screen">
+      <h1 className="text-2xl font-bold uppercase mb-3 tracking-[0.16em] text-orange-700">
+        Tasks
+      </h1>
+      <div className="bg-amber-100 w-60 p-3">
+        <p className="text-lg font-semibold text-stone-950 mb-3">
+          Select your view mode
+        </p>
+        <button
+          onClick={() => setView("list")}
+          className={`px-3 py-1 border rounded transition cursor-pointer mr-4 ${
+            view === "list"
+              ? "bg-blue-500 text-white border-blue-500"
+              : "bg-white text-gray-700 border-gray-300"
+          }`}
+        >
+          List View
+        </button>
+        <button
+          onClick={() => setView("card")}
+          className={`px-3 py-1 border rounded transition cursor-pointer ${
+            view === "card"
+              ? "bg-blue-500 text-white border-blue-500"
+              : "bg-white text-gray-700 border-gray-300"
+          }`}
+        >
+          Card View
+        </button>
       </div>
 
-     
-      <TaskList tasks={filteredTasks} />
+      <div className="flex mb-6 mt-4 justify-between rounded-[1.75rem] border border-stone-800/10 bg-white/75 p-6 shadow-sm">
+        {["All", "Completed", "In Progress", "Pending", "Overdue"].map(
+          (status) => (
+            <button
+              key={status}
+              type="button"
+              aria-pressed={filter === status}
+              onClick={() => setFilter(status)}
+              className={`px-4 py-1 rounded-full font-medium border cursor-pointer ${
+                filter === status
+                  ? "bg-blue-500 text-white border-blue-500"
+                  : "bg-white text-gray-700 border-gray-300"
+              }`}
+            >
+              {status}
+            </button>
+          ),
+        )}
+      </div>
+
+      <TaskList tasks={filteredTasks} onDelete={deleteTasks} view={view} />
     </div>
   );
 }
